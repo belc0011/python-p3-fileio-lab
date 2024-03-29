dfca88b5d2e3fe122eec7ceb61e33b11ae6eb35e
Monica Garcia <monicairenegarcia@gmail.com>
2024-03-29 14:03:44 -0700

    complete fileio lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,17 @@
+from pathlib import Path
 def write_file(file_name, file_content):
-    pass
+    new_file_name=str(file_name)
+    with open(new_file_name + '.txt', 'w') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    new_file_name = str(file_name)
+    with open(new_file_name + '.txt', 'a') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    new_file_name = str(file_name)
+    with open(new_file_name + '.txt', 'r') as file:
+        file_contents = file.read()
+        return file_contents
+
